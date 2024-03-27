3ed7a28e5ac5896034e032583ed9c0010bf82431
gebmecod <jybravo@up.edu.ph>
2024-03-27 16:23:37 +0800

    update key generator to create file

--- generate_key.py
+++ generate_key.py
@@ -1,12 +1,15 @@
 from Crypto.PublicKey import RSA
 
 class Key():
-    def __init__(self) -> None:
+    def __init__(self, filename) -> None:
         self.key = RSA.generate(2048)
+        self.filename = filename
 
     def public_key(self):
-        return self.key.publickey().export_key()
+        with open(f'{self.filename}.pub', 'wb') as f:
+            f.write(self.key.public_key().export_key())
     
     def private_key(self):
-        return self.key.export_key()
+        with open(self.filename, 'wb') as f:
+            f.write(self.key.export_key())
 
