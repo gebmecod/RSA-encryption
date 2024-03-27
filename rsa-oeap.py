8f09fdee5e7d1f7eebe81f0e17092889390d0d4e
gebmecod <jybravo@up.edu.ph>
2024-03-27 09:18:23 +0800

    include sign and verify function

--- rsa-oeap.py
+++ rsa-oeap.py
@@ -1,11 +1,17 @@
 from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 from generate_key import Key
 
 m = b'You can attack now'
-key_pair = Key()
-public_key = key_pair.public_key()
-private_key = key_pair.private_key()
+encryption_key = Key()
+encryption_public_key = encryption_key.public_key()
+encryption_private_key = encryption_key.private_key()
+
+signing_key = Key()
+signing_public_key = signing_key.public_key()
+signing_private_key = signing_key.private_key()
 
 def encrypt_message(m, k):
 
@@ -15,6 +21,15 @@ def encrypt_message(m, k):
     
     return ciphertext
 
+def sign_ciphertext(c, k):
+
+    key = RSA.importKey(k)
+    hash = SHA256.new(c)
+    signer = pkcs1_15.new(key)
+    signature = signer.sign(hash)
+
+    return signature
+
 
 def decrypt_message(c, k):
 
@@ -22,9 +37,21 @@ def decrypt_message(c, k):
     cipher = PKCS1_OAEP.new(key)
     message = cipher.decrypt(c)
 
-    return message
+    print(message.decode())
 
-c = encrypt_message(m, public_key)
-message = decrypt_message(c, private_key)
+def verify_message(s, c, k):
 
-print(message)
+    signature = s
+    key = RSA.import_key(k)
+    hash = SHA256.new(c)
+
+    try:
+        pkcs1_15.new(key).verify(hash, signature)
+        print("Signature is valid.")
+        decrypt_message(c, encryption_private_key)
+    except(ValueError, TypeError):
+        print("Message has been tampered")
+
+ciphertext = encrypt_message(m, encryption_public_key)
+signature = sign_ciphertext(ciphertext, signing_private_key)
+verify_message(signature, ciphertext, signing_public_key)
