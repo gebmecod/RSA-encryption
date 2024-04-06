a1ae39e7e3bd31dd841404874e67e9e219c76c97
gebmecod <jybravo@up.edu.ph>
2024-04-06 10:55:28 +0800

    fix input formatting

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     interactive.generate_key()
     interactive.create_message()
     interactive.encrypt_then_sign()
-    tamper = input('Do you want to tamper the encrypted message?[y/n]')
+    tamper = input('Do you want to tamper the encrypted message?[y/n]: ')
     if tamper == 'y':
         interactive.tamper_encrypted_message()
     interactive.verify_then_decrypt()
